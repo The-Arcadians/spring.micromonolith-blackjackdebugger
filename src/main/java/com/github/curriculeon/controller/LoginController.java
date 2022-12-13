b17b7adbcae885c6e64d3f0003f27fdf3c0de68d
randy <randyr_1993@yahoo.com>
2022-12-12 21:08:35 -0500

    Changes to LoginController

--- src/main/java/com/github/curriculeon/controller/LoginController.java
+++ src/main/java/com/github/curriculeon/controller/LoginController.java
@@ -1,32 +1,28 @@
 package com.github.curriculeon.controller;
 
-import com.github.curriculeon.model.Person;
-import com.github.curriculeon.service.PersonService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
+import com.github.curriculeon.model.User;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @CrossOrigin(origins = "*")
 @RequestMapping(value = "/login-controller")
 @RestController
 public class LoginController {
-
     private User currentLoggedInUser;
 
-    public ResponseEntity<String> getCurrentUser(){
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public ResponseEntity<User> getCurrentUser(){
         return new ResponseEntity<>(this.currentLoggedInUser, HttpStatus.OK);
     }
 
     @RequestMapping(value = "/login/{name}", method = RequestMethod.PUT)
-    public ResponseEntity<Person> login(@PathVariable String name) {
-        this.currentLoggedInUser = name;
-        ResponseEntity<User> responseEntity = new ResponseEntity<>(currentLoggedInUser, HttpStatus.OK);
-        return responseEntity;
+    public ResponseEntity<User> login(@PathVariable String name) {
+        System.out.println("Logging in..");
+        this.currentLoggedInUser = new User(name);
+        ResponseEntity<User> response = new ResponseEntity<>(currentLoggedInUser, HttpStatus.OK);
+        return response;
     }
 
 }
