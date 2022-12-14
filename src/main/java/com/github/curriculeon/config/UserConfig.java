84e78fbf2ead65ec3a9a5a59327f452b01c9d305
randy <randyr_1993@yahoo.com>
2022-12-14 12:47:15 -0500

    modified UserConfig class to match UserService methods

--- src/main/java/com/github/curriculeon/config/UserConfig.java
+++ src/main/java/com/github/curriculeon/config/UserConfig.java
@@ -1,7 +1,6 @@
 package com.github.curriculeon.config;
 
 import com.github.curriculeon.model.User;
-import com.github.curriculeon.model.Winner;
 import com.github.curriculeon.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
@@ -16,11 +15,9 @@ public class UserConfig {
 
     @PostConstruct
     public void setup(){
-        service.create(new User(1L, "Randy", "Curriculeon"));
-        service.create(new User(2L, "Leon", "Curriculeon"));
-        service.create(new User(3L, "Brent", "Curriculeon"));
-        service.create(new User(4L, "Eric", "Curriculeon"));
-
-
+        service.create(new User("Randy", "Curriculeon"));
+        service.create(new User("Leon", "Curriculeon"));
+        service.create(new User("Brent", "Curriculeon"));
+        service.create(new User("Eric", "Curriculeon"));
     }
 }
