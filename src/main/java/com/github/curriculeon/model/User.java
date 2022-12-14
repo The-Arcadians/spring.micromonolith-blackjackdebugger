ecab0b4d07060063dc845f47b74281e82caf958c
randy <randyr_1993@yahoo.com>
2022-12-14 12:58:18 -0500

    minor changes to User class

--- src/main/java/com/github/curriculeon/model/User.java
+++ src/main/java/com/github/curriculeon/model/User.java
@@ -1,21 +1,59 @@
 package com.github.curriculeon.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class User {
-    private String name;
 
-    public User(){
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
+    private String username;
+    private String password;
+
+    public User() {
+        // Default constructor
     }
 
-    public User(String name){
-        this.name = name;
+    public User(String username){
+        this.username = username;
     }
 
-    public String getName(){
-        return name;
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
     }
 
-    public void setName(String name){
-        this.name = name;
+    public User(Long id, String username) {
+        this.id = id;
+        this.username = username;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
     }
 }
