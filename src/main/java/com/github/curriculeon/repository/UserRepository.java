a3872f11ee4eba0772449b93d0b9063b82da929e
randy <randyr_1993@yahoo.com>
2022-12-14 12:59:39 -0500

    UserRepository now extends JpaRepository

--- src/main/java/com/github/curriculeon/repository/UserRepository.java
+++ src/main/java/com/github/curriculeon/repository/UserRepository.java
@@ -1,7 +1,8 @@
 package com.github.curriculeon.repository;
 
 import com.github.curriculeon.model.User;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
+
 import org.springframework.stereotype.Repository;
 
 /** This is the class that allows us to speak to the database
@@ -9,7 +10,7 @@ import org.springframework.stereotype.Repository;
  * Including: saving, deleting, finding "User" entities.
  */
 @Repository
-public interface UserRepository extends CrudRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
     User findByUsernameAndPassword(String username, String password);
     /*This method is used to query the database for a user with a given username and password.
     The 'LoginController' uses this class to determine if a user with the given username and password exists in the database.
