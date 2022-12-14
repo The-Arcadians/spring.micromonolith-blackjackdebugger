314fb8b8b12692914d26612d9db9607f3cd769b2
randy <randyr_1993@yahoo.com>
2022-12-14 08:24:45 -0500

    Added helpful commments to explain the UserRepository class

--- src/main/java/com/github/curriculeon/repository/UserRepository.java
+++ src/main/java/com/github/curriculeon/repository/UserRepository.java
@@ -1,2 +1,17 @@
-package com.github.curriculeon.repository;public class UserRepository {
+package com.github.curriculeon.repository;
+
+import com.github.curriculeon.model.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+/** This is the class that allows us to speak to the database
+ * By extending CrudRepository, PersonRepository inherits several methods for working with "User" persistence.
+ * Including: saving, deleting, finding "User" entities.
+ */
+@Repository
+public interface UserRepository extends CrudRepository<User, Long> {
+    User findByUsernameAndPassword(String username, String password);
+    /*This method is used to query the database for a user with a given username and password.
+    The 'LoginController' uses this class to determine if a user with the given username and password exists in the database.
+    */
 }
